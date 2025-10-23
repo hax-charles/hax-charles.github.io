edcaee3ca4617d18657d465208c7c26ddee8dfc1
Charles Peter <charles.peter@sosv.com>
2025-10-23 14:23:46 +0530

    updated inventory app

--- pages/inventory-app/vite.config.ts
+++ pages/inventory-app/vite.config.ts
@@ -1,23 +1,32 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+// import path from 'path';
+// import { defineConfig, loadEnv } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+// export default defineConfig(({ mode }) => {
+//     const env = loadEnv(mode, '.', '');
+//     return {
+//       server: {
+//         port: 3000,
+//         host: '0.0.0.0',
+//       },
+//       plugins: [react()],
+//       define: {
+//         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+//         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+//       },
+//       resolve: {
+//         alias: {
+//           '@': path.resolve(__dirname, '.'),
+//         }
+//       }
+//     };
+// });
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/hax-charles.github.io/pages/inventory-app/' // IMPORTANT: Replace YOUR_REPO_NAME with your repository name
+})
